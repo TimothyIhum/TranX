91faab33ae6d347b4e4d0a8f447d854d232c7327
Timothy <timothyihum@gmail.com>
2026-07-16 16:18:09 +0100

    Remove all project files and directories

--- lib/constants.ts
+++
@@ -1,7 +0,0 @@
-export const VAULT_CATEGORIES = [
-  "Passwords", "PINs", "Bank Details", "Credit Cards", "Recovery Codes",
-  "Private Notes", "IDs", "Contacts", "Licenses", "Crypto Wallets",
-  "API Keys", "Server Credentials", "Software Keys", "Medical Information",
-  "Personal Documents", "Wi-Fi Credentials", "Email Accounts",
-  "Social Media Accounts", "Business Secrets", "Other",
-];
